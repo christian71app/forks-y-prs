4234c03906a059eb4a90d5853a2f5dfafb1a6395
Christian Tineo Villon <chris17tineo@gmail.com>
2025-08-28 20:44:18 -0500

    tests chequeados

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ class Product {
   id: number;
   name: string;
   price: number;
+
+  static findProductsBelow(precioBase: number) {
+    return products.filter(product => product.price < precioBase);
+  }
 }
 
 class User {
@@ -21,8 +25,11 @@ class User {
   }
   addProducts(newProducts: Product[]) {
     // esto no funciona:
-    this.products.push(newProducts);
+    // this.products.push(newProducts);
     // pista: push no suma muchos items (agrega de a uno)
+    newProducts.forEach(product => {
+      this.products.push(product);
+    })
   }
 }
 
